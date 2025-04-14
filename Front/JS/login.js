17753f162570c57d0ce575075a69010677e3533e
JulianaNishimura <juliana.nishimura.tds24@gmail.com>
2025-04-14 17:28:07 -0300

    Login validando

---
+++ Front/JS/login.js
@@ -0,0 +1,23 @@
+function Verificar(){
+    let email = document.getElementById('emailLogin');
+    let senha = document.getElementById('passwordLogin');
+
+    fetch(`http://localhost:8080/professores/get/${email}`)
+    .then(response => {
+        if(!response.ok){
+            throw new Error("Não existe um usuário com esse email");
+        }
+        return response.json();
+    })
+    .then(professor => {
+        console.log(professor)
+        Compara(professor, senha);
+    })
+    .catch(erro => {
+        console.error(erro);
+    })
+}
+
+function Compara(professor, senha){
+    
+}
