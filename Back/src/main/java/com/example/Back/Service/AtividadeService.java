cbf66a6dc1cfcab05596082b0ed84ebea68f46d9
JulianaNishimura <juliana.nishimura.tds24@gmail.com>
2025-04-14 16:56:34 -0300

    Back

---
+++ Back/src/main/java/com/example/Back/Service/AtividadeService.java
@@ -0,0 +1,43 @@
+package com.example.Back.Service;
+
+import com.example.Back.entity.Atividade;
+import com.example.Back.entity.Turma;
+import com.example.Back.repository.AtividadeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AtividadeService {
+
+    @Autowired
+    private AtividadeRepository atividadeRepository;
+
+    public String salvarAtividade(Atividade atividade) {
+        atividadeRepository.save(atividade);
+        return "Atividade cadastrada";
+    }
+
+    public List<Atividade> listarAtividade() {
+        return atividadeRepository.findAll();
+    }
+
+    public String atualizarAtividade(Long id, Atividade atividade) {
+        if(atividadeRepository.existsById(id)){
+            Atividade atividadeExistente = atividadeRepository.findById(id).get();
+            atividadeExistente.setDescricão(atividade.getDescricão());
+            atividadeRepository.save(atividadeExistente);
+            return "Atualizado com sucesso";
+        }
+        return "Erro ao atualizar";
+    }
+
+    public String deletarAtividade(Long id) {
+        if(atividadeRepository.existsById(id)){
+            atividadeRepository.deleteById(id);
+            return "deletado com sucesso";
+        }
+        return "Erro ao deletar";
+    }
+}
---
+++ Back/src/main/java/com/example/Back/repository/AtividadeRepository.java
@@ -0,0 +1,7 @@
+package com.example.Back.repository;
+
+import com.example.Back.entity.Atividade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AtividadeRepository extends JpaRepository<Atividade,Long> {
+}
