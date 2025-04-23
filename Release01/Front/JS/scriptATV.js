e249b1d9c294269108c0a0bbb011709c83197638
MykyBalb <mykaelbalbino06@gmail.com>
2025-04-23 16:07:17 -0300

    front mais ou menos feito

---
+++ Release01/Front/JS/scriptATV.js
@@ -0,0 +1,11 @@
+function showModal() {
+  document.getElementById('atividadeModal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('atividadeModal').style.display = 'none';
+}
+
+function deleteAtividade() {
+    alert('Atividade excluída!');
+}
