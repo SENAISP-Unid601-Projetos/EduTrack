02868d83cd2b7a8ae221967347e2000e81cc6248
MykyBalb <mykaelbalbino06@gmail.com>
2025-04-26 12:34:21 -0300

    Get Alunos

--- Victor_Mykael_Miguel_Felipe/Front/JS/script.js
+++ Victor_Mykael_Miguel_Felipe/Front/JS/script.js
@@ -1,24 +1,35 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://10.110.12.6:8080',
-    headers: {
-        "Content-Type": "application/json"
-    }
+  baseURL: 'http://10.110.12.6:8080',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 async function postTurma(nome, idProfessor) {
-    try {
-        const data = { nome, idProfessor };
-        const response = await instance.post('/turmas', data);
-        if (response.status === 200) {
-            return response.data;
-        }
-    } catch (err) {
-        console.error("error", err);
-        throw err;
+  try {
+    const data = { nome, idProfessor };
+    const response = await instance.post('/turmas', data);
+    if (response.status === 201) {
+      return response.data;
     }
+  } catch (err) {
+    console.error('error', err);
+    throw err;
+  }
 }
 
+async function getAlunos() {
+  try {
+    const response = await instance.get('/alunos');
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (err) {
+    console.error('error', err);
+    throw err;
+  }
+}
 
-
+console.log(getAlunos());
