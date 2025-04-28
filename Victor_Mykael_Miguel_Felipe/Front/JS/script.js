7693c19a8278f06e3b1825a87b97121e3e215a5c
MykyBalb <mykaelbalbino06@gmail.com>
2025-04-28 14:11:24 -0300

    CDN Axios

--- Victor_Mykael_Miguel_Felipe/Front/JS/script.js
+++ Victor_Mykael_Miguel_Felipe/Front/JS/script.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://10.110.12.6:8080',
+  baseURL: 'http://10.110.12.52:8080',
   headers: {
     'Content-Type': 'application/json',
   },
