346b3c59f43f4c2be727f3d68696498e1c9c930a
KauaFrenedozo10 <kfrenedoso@gmail.com>
2025-04-28 14:07:53 -0300

    JScompleto

--- Release01/Front/JS/login.js
+++ Release01/Front/JS/login.js
@@ -1,32 +1,47 @@
-const response = axios.create( {
+const axiosInstance = axios.create({
   baseURL: "http://10.110.12.52:8080",
   headers: {
-   "Content-Type": "application/json"
+    "Content-Type": "application/json",
   }
-  
-})
+});
 
+// Função de login
+async function login(email, senha) {
+  try {
+    const response = await axiosInstance.post('API/login', {
+      email: email,
+      senha: senha
+    });
 
-async function login(email,senha) {
- try {
-   const response = await response.post ('API', {
-     email: email,
-     senha: senha
-   })
+    if (response.status === 200) {
+      console.log('Login Bem Sucedido!');
+      console.log('Dados retornados:', response.data);
+      return response.data;
+    } else {
+      console.log('Algo deu errado:', response.status);
+    }
 
-   if(response === 200) {
-     console.log('Login Bem Sucedido!');
-     console.log('Dados retornados:', response.data);
-     
-     return response.data;
-   } else {
-       console.log('algo deu errado:', response.status)
-   }
-
- }catch(err) {
-   console.error("Erro ao tentar login: ", err.response?.data || err.message)
- }
- 
- 
+  } catch (err) {
+    console.error("Erro ao tentar login: ", err.response?.data || err.message);
+  }
 }
-login('Email', 'Senha')
+
+// Função de cadastro
+async function cadastrar(email, senha) {
+  try {
+    const response = await axiosInstance.post('API/cadastro', {
+      email: email,
+      senha: senha,
+    });
+
+    if (response.status === 201) {
+      console.log('Cadastro bem sucedido!');
+      console.log('Dados retornados:', response.data);
+      return response.data;
+    } else {
+      console.log('Erro no cadastro:', response.status);
+    }
+  } catch (err) {
+    console.error("Erro ao tentar cadastrar: ", err.response?.data || err.message);
+  }
+}
