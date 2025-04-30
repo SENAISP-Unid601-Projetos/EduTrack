354935789893e781dcd09479b161b41c7ac1cff2
Vinicius dos Santos <vinistos@gmail.com>
2025-04-30 17:19:00 -0300

    Update TurmaController.java

--- Release01/Back/src/main/java/com/example/Back/controller/TurmaController.java
+++ Release01/Back/src/main/java/com/example/Back/controller/TurmaController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/turmas")
+@RequestMapping("/turma")
 public class TurmaController {
     @Autowired
     private TurmaService turmaService;
